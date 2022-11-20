be8646069cbdc169eecf72afc0c4bb505d0aaa3a
Silimon4ikGitHub <115254695+Silimon4ikGitHub@users.noreply.github.com>
2022-11-20 18:31:35 +0100

    Pins Moving and Stop

--- Assets/Scripts/PinScript.cs
+++ Assets/Scripts/PinScript.cs
@@ -4,32 +4,52 @@ using UnityEngine;
 
 public class PinScript : MonoBehaviour
 {
+    [Header ("Pin Parameters")]
     [SerializeField] private  float speed = 1;
+    [SerializeField] private  bool stop = false;
+    
+    [Header ("Only for Cheking")]
     [SerializeField] private  bool imIn;
     [SerializeField] private Vector3 pinPSN;
-    [SerializeField] private CellCreator pins;
+    [SerializeField] private CellCreator cells;
     [SerializeField] private  GameObject cell; 
     
     void Awake()
     {
-        pinPSN = transform.position;
+        
         cell = GameObject.Find("Cells");
-        pins = cell.GetComponent<CellCreator>();
+        cells = cell.GetComponent<CellCreator>();
     }
     void Update()
     {
-        
-        for (int i=0; i < pins.cellsObjects.Length; i++)
-        {
+        pinPSN = transform.position;
+        //for (int i=0; i < pins.cellsObjects.Length; i++)
+        //{
             
-        if (pinPSN == pins.cellsObjects[i].transform.position)
-         {
-            imIn = true;
-         }
+        //if (pinPSN == pins.cellsObjects[i].transform.position)
+        // {
+        //    imIn = true;
+        //}
+        //
+        if(pinPSN.y <= cells.cellsObjects[1].transform.position.y)
+        {
+            StopMoution();
+
         }
     }
     void FixedUpdate()
     {
         transform.position = new Vector3 (transform.position.x, transform.position.y + speed * Time.deltaTime, transform.position.z);
+
+        if (imIn == true)
+        {
+            StopMoution();
+        }
+    }
+
+    void StopMoution()
+    {
+        speed = 0;
+
     }
 }
