22180ca51d14a3f1b7cb59f0f51151297bec3bc8
RomanKapustinskyi <kapustinskyiroman@gmail.com>
2022-11-25 23:24:08 +0200

    Refactor :)

--- Assets/Scripts/PinScriptNew.cs
+++ Assets/Scripts/PinScriptNew.cs
@@ -6,7 +6,7 @@ public class PinScriptNew : MonoBehaviour
 {
     [SerializeField] private float speed = 0.02f;
 
-    [Header ("For Check Only")]
+    [Header("For Check Only")]
     [SerializeField] private int myPSNinArray;
     [SerializeField] private Vector3 pinPSN;
     [SerializeField] private Vector3 cellPSN;
@@ -20,9 +20,9 @@ public class PinScriptNew : MonoBehaviour
 
 
 
-    private void Awake() 
+    private void Awake()
     {
-        cellCreatorGM = GameObject.Find("Cells");
+        cellCreatorGM = GameObject.Find("Cells"); // Bad practice to use Find, add Cells throw inspector
         cellCreatorScript = cellCreatorGM.GetComponent<CellCreator>();
 
         pinsCreatorGM = GameObject.Find("Pins");
@@ -37,36 +37,48 @@ public class PinScriptNew : MonoBehaviour
 
         pinsCreatorScript.pinsObjects[myPSNinArray] = transform.gameObject;
         currentCell = cellCreatorScript.cellsObjects[myPSNinArray].GetComponent<CellScript>();
-        
+
         GoToCell();
 
         CheckNextCell();
-            
+
     }
     void GoToCell()
     {
+        // Why I need this comment? Why may I just name this method "MoveToNextCell()" ? 
+        // Also don't use magic numbers
+        // Instead of "speed = 0.02f" right here, use constants
+        // Or if you use Inspector, don't stop falling throw speed
+        // use private bool _isPlay or _isStoped
+
         //======PIN GOIND IN DIRRECTION OF targetCell======
         speed = 0.02f;
         targetCell = cellCreatorScript.cellsObjects[myPSNinArray].transform.position;
-        transform.position = Vector3.MoveTowards(transform.position, targetCell, speed );
+        transform.position = Vector3.MoveTowards(transform.position, targetCell, speed);
     }
 
     void Stop()
     {
-        speed = 0;  
+        speed = 0;
     }
     void CheckNextCell()
     {
-     //======CHECK FOR LOCATION IN FIELD======
+        //Don't use comments for that things
+        //Clear code - code that any coder able to understand without comments
+        //for i.g. myPSNinArray? Why? Position of what in array?
+        // currentArrayPosition or currentArrayIndex or currentRowIndex or something like that more clear
+        // Also you can do it in other method see **
+
+        //======CHECK FOR LOCATION IN FIELD======
         if (myPSNinArray >= pinsCreatorScript.rows)
         {
             nextCell = cellCreatorScript.cellsObjects[myPSNinArray - pinsCreatorScript.rows].GetComponent<CellScript>();
-        //======CHECK WHEN INSIDE OF CELL======
-            if (pinPSN == cellPSN)
+            //======CHECK WHEN INSIDE OF CELL======
+            if (pinPSN == cellPSN) //Don't use "PSN", use full names like "cellPosition"
             {
                 pinsCreatorScript.pinsObjects[myPSNinArray] = transform.gameObject;
-        //======CHECK NEXT CELL IS EMPTY=======      
-                if (nextCell.isFull == false)
+                //======CHECK NEXT CELL IS EMPTY=======      
+                if (nextCell.isFull == false) //Don't use "== false", use "!nextCell.isFull"
                 {
                     myPSNinArray = myPSNinArray - pinsCreatorScript.rows;
                 }
@@ -74,4 +86,47 @@ public class PinScriptNew : MonoBehaviour
             }
         }
     }
+
+    //** private bool IsLocationInField(int rowIndex) => myPSNinArray >= pinsCreatorScript.rows;
+    // Then you use it like that "if (IsLocationInField(currentRowIndex))"
+
+    // Also you can simplifie your code:
+    //
+    //    if (myPSNinArray >= pinsCreatorScript.rows)
+    //        if (pinPSN == cellPSN)
+    //            if (nextCell.isFull == false)
+    //            {
+    //                nextCell = cellCreatorScript.cellsObjects[myPSNinArray - pinsCreatorScript.rows].GetComponent<CellScript>();
+    //                pinsCreatorScript.pinsObjects[myPSNinArray] = transform.gameObject;
+    //                myPSNinArray = myPSNinArray - pinsCreatorScript.rows;
+    //            }
+    //
+    // Or:
+    //
+    //    if (myPSNinArray <= pinsCreatorScript.rows)
+    //        return;
+    //    if (pinPSN != cellPSN)
+    //        return;
+    //
+    //    if (nextCell.isFull == false)
+    //    {
+    //        nextCell = cellCreatorScript.cellsObjects[myPSNinArray - pinsCreatorScript.rows].GetComponent<CellScript>();
+    //        pinsCreatorScript.pinsObjects[myPSNinArray] = transform.gameObject;
+    //        myPSNinArray = myPSNinArray - pinsCreatorScript.rows;
+    //    }
+    //
+    // The best way:
+    //
+    //
+    //    if (!IsLocationInField(currentRowIndex) || !IsInsideOfCell())
+    //        return;
+    //
+    //    if (!nextCell.isFull)
+    //    {
+    //        nextCell = cellCreatorScript.cellsObjects[myPSNinArray - pinsCreatorScript.rows].GetComponent<CellScript>();
+    //        pinsCreatorScript.pinsObjects[myPSNinArray] = transform.gameObject;
+    //        myPSNinArray = myPSNinArray - pinsCreatorScript.rows;
+    //    }
+    //
+    // Is it shorter and cleaner?)
 }
